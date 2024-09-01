c41178fed5909a88d053887f1b8f91a5a651d129
quantum-corn <akashkhetan044@gmail.com>
2024-09-01 21:32:26 +0530

    Add resolution change button

--- sketch.js
+++ sketch.js
@@ -1,4 +1,10 @@
-const cellBehaviour = function (cell) {
+const cellBehaviour = function (cell, size) {
+    const sketchSize = 640;
+    let cellSize = sketchSize / size;
+    cellSize += "px";
+
+    cell.style.width=cellSize;
+    cell.style.height=cellSize;
     cell.addEventListener("mouseover", e=>etch(e));
 }
 
@@ -6,30 +12,44 @@ const etch = function (e) {
     e.target.style.backgroundColor="black";
 }
 
-const sketchSize = 640;
-let size = 16;
-let cellSize = sketchSize / size;
-cellSize += "px";
-
-const main = document.querySelector(".main");
-let row, cell, sketch;
-
-sketch=document.createElement("div");
-for (let i = 0; i < size; i++) {
-    row=document.createElement("div");
-    for (let j = 0; j < size; j++) {
-        cell=document.createElement("div");
-        cell.className="cell";
-        row.appendChild(cell);
-    }
-    row.className="row";
-    sketch.appendChild(row);
+const changeSize = function (size) {
+    if (size>=1 && size<=100) sketchpad(size);
+    else getSize();
 }
-sketch.className="sketch";
 
-cell=sketch.querySelectorAll(".cell");
-console.dir(cell);
-cell.forEach(box=>cellBehaviour(box));
+const getSize = function() {
+    changeSize(prompt("Enter the grid size, up to 100. Eg: for a grid of 100x100 cells, enter 100"));
+}
 
-main.appendChild(sketch);
+const sketchpad = function (size) {
+    const main = document.querySelector(".main");
+    let row, cell, sketch;
+
+    sketch=document.createElement("div");
+    for (let i = 0; i < size; i++) {
+        row=document.createElement("div");
+        for (let j = 0; j < size; j++) {
+            cell=document.createElement("div");
+            cell.className="cell";
+            row.appendChild(cell);
+        }
+        row.className="row";
+        sketch.appendChild(row);
+    }
+    sketch.className="sketch";
+
+    cell=sketch.querySelectorAll(".cell");
+    console.dir(cell);
+    cell.forEach(box=>cellBehaviour(box, size));
+
+    let oldSketch = main.querySelector(".sketch")
+    if(oldSketch)main.removeChild(oldSketch);
+
+    main.appendChild(sketch);
+}
+
+sketchpad(16);
+
+let button = document.querySelector(".gridsize");
+button.addEventListener("click", e=>getSize());
 
